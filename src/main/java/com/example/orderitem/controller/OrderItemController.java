dfa1432ba2eabff23b5e1cc6d82aecf4160f4ea2
Faiz <faizmajeed21@gmail.com>
2020-09-09 10:14:12 +0530

    generalizing the url

--- src/main/java/com/example/orderitem/controller/OrderItemController.java
+++ src/main/java/com/example/orderitem/controller/OrderItemController.java
@@ -12,28 +12,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.orderitem.service.OrderItemService;
 import com.example.orderitem.vo.OrderItemVo;
 
 @RestController
+@RequestMapping("orderitem")
 public class OrderItemController {
 	
 	@Autowired
 	private OrderItemService service;
 
-	@GetMapping("/getDetailsById/{orderId}")
+	@GetMapping("/{orderId}")
 	public ResponseEntity<Object> getOrderById(@PathVariable Long orderId){
 		return new ResponseEntity<Object>(service.getOrderById(orderId), HttpStatus.OK);
 	}
 	
-	@PostMapping("/save")
+	@PostMapping
 	public ResponseEntity<Object> saveOrder(@Valid @RequestBody List<OrderItemVo> order){
 		return new ResponseEntity<Object>(service.saveOrder(order),HttpStatus.OK);
 	}
 	
-	@DeleteMapping("/delete/{orderId}")
+	@DeleteMapping("/{orderId}")
 	public ResponseEntity<Object> deleteOrder(@PathVariable Long orderId){
 		return new ResponseEntity<Object>(service.deleteOrder(orderId) ,HttpStatus.OK);
 	}
